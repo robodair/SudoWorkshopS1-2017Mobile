4ae9300b219042b9561562f6a50523d00705eb60
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-03-26 15:28:52 +1100

    Add Posts page and provider to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { LoginPage } from '../pages/login/login';
+import { PostsPage } from '../pages/posts/posts';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { PostsProvider } from '../providers/posts-provider';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
-    LoginPage,
+    PostsPage,
     TabsPage
   ],
   imports: [
@@ -25,13 +27,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     AboutPage,
     ContactPage,
-    LoginPage,
+    PostsPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PostsProvider
   ]
 })
 export class AppModule {}
